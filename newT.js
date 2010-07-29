34efed94515a15a9b5b0333e217afdf93a1bda6c
Jeffrey Tierney <jeff@new-host.home>
2010-07-28 23:07:00 -0400

    some high-level comments

--- newT.js
+++ newT.js
@@ -1,5 +1,10 @@
 (function (temp) {
+  // Default global var name will be newT
+  // can be overridden by passing something different
+  // into the self executing wrapper function
   temp = temp || "newT";
+
+  // internally refer to it as $ for brevity sake
   var $ = function() {
     this.init();
   }
@@ -11,9 +16,14 @@
       this.__createMethods();
       return this;
     },
+    // simple function to save the passed in template
     save: function(name, template) {
       this.templates[name] = template;
+      return this;
     },
+    // create the elements for the template
+    // and if an exisiting root el was passed in, append it to that root
+    // either way, return the newly created element(s)
     render: function(name, data, el) {
       var new_el = this.templates[name](data);
 
@@ -22,6 +32,9 @@
       }
       return new_el;
     },
+    // function to iterate over a collection and render a template
+    // for each item in the collection
+    // uses a document fragment to collect each element and pass it back
     each_render: function(data, template) {
       var frag = document.createDocumentFragment();
       for(var i in data) {
@@ -31,6 +44,9 @@
       }
       return frag;
     },
+    // more free form iterator function that allows passing an ad-hoc
+    // rendering function to be evaluated for each item in the collection
+    // uses a document fragment to collect each element and pass it back
     each: function(data, func) {
       var frag = document.createDocumentFragment();
       for(var i in data) {
@@ -40,6 +56,9 @@
       }
       return frag;
     },
+    // function that gets called in initializing the class... loops through
+    // list of allowed html elements, and creates a helper function on the prototype
+    // to allow easy creation of that element simply by calling its name as a function
     __createMethods: function() {
       var el_list = "a abbr acronym address applet area b base basefont bdo bgsound big blockquote body br button caption center cite code col colgroup comment custom dd del dfn dir div dl dt em embed fieldset font form frame frameset head hn hr html i iframe img input input type=button input type=checkbox input type=file input type=hidden input type=image input type=password input type=radio input type=reset input type=submit input type=text ins isindex kbd label legend li link listing map marquee menu meta nobr noframes noscript object ol optgroup option p param plaintext pre q rt ruby s samp script select small span strike strong style sub sup table tbody td textarea tfoot th thead title tr tt u ul var wbr xml xmp";
       var els = el_list.split(" ");
@@ -54,6 +73,13 @@
       })(els[i]);
       return this;
     },
+    // generic version of the function used to build the element specific creation functions
+    // type -> name of element to create
+    // attributes (optional) -> object with key/value pairs for attributes to be added to the element
+    //                          to avoid silliness with using class as an object key
+    //                          you must use "clss" to set class.  yuck
+    // content (optional) -> arbitrarily many pieces of content to be added within the element
+    //                       can be strings, domElements, or anything that evaluates to either of those
     __createElGeneric: function(type, attributes, content) {
       var args = Array.prototype.slice.call(arguments).slice(1);
       var el = document.createElement(type);
@@ -82,9 +108,9 @@
         }
       }
 
-
       for(var i=0, len=content.length; i<len; i++) {
         if(typeof content[i] === "string") {
+          // if the content is a string, create a Text Node to hold it and append
           el.appendChild(document.createTextNode(content[i]));
         }
         else if(typeof content[i] === "function") {
