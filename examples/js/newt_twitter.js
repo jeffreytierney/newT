82c9cddd124ee72ea7d092ca3ee406ca9d9408d5
gregory80 <gregory.tomlinson@gmail.com>
2011-06-25 16:23:44 -0400

    cleanup twitter widget some

--- examples/js/newt_twitter.js
+++ examples/js/newt_twitter.js
@@ -1,24 +1,19 @@
 /*
-*   newt_twitter
-*
+*   newt_twitter.js
+*       
+*   depndencies:
+*       jQuery-1.5+
+*       newT 1.0+
 * */
 
 (function(window, undefined) {
     
     var nT = function() {
-        this.init();
+        this.init(); // fire this automatically
         return this;
     }
     nT.prototype = {
-        templates : {
-            box : "tweet_box",
-            tweet : null
-        },
-        init : function() {
-            this.dom_box();
-            this.dom_tweet();
-        },
-        
+        // display newt_twitter widget username stream
         show : function( el, username, count ) {
             var self=this, 
                 promise=self.remote( username, count );
@@ -28,13 +23,25 @@
                     "el" : el
                 });
             });
+            return self; // you can chain
+        },
+        // the names of the templates newt_twitter uses
+        templates : {
+            box : "tweet_box",
+            tweet : "single_tweet"
+        },
+        // setup the newT templates
+        init : function() {
+            this.dom_box();
+            this.dom_tweet();
         },
         dom_box : function() {
-            newT.save("tweet_box", function(data){
+            var self=this;
+            newT.save(self.templates.box, function(data){
                 return (
                     newT.div({clss : "tweet_box_shell"},
                         newT.div( 
-                            newT.eachRender( data, "single_tweet" )
+                            newT.eachRender( data, self.templates.tweet )
                         )
                     )
                 )
@@ -42,33 +49,37 @@
         },
         dom_tweet : function() {
             
-            newT.save("single_tweet", function(tweet) {
-                var profile_url =  "http://twitter.com/" + tweet.user.screen_name;
+            newT.save(this.templates.tweet, function(tweet) {
+                var profile_url =  "http://twitter.com/" + tweet.user.screen_name,
+                    permalink="http://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str; 
                 return (
                   newT.div({clss : "single_tweet_box"},
+                    // photo
                     newT.div({ clss : "user_profile" },
                         newT.a({href : profile_url},
                             newT.img({src : tweet.user.profile_image_url})
-
                         )
                     ),
+                    // screen name, tweet text, time
                     newT.div({ clss : "user_meta" },
                         newT.h4({}, 
                             newT.a({href : profile_url}, tweet.user.screen_name )
                         ),
                         newT.h3({}, tweet.text),
                         newT.div({ clss : "tweet_time" },
-                            newT.div(tweet.created_at)
+                            newT.a({ href : permalink },
+                                newT.div(tweet.created_at)
+                            )
                         )
-
                     ),
+                    // just a  little clear for this box
                     newT.div({clss : "hr"}, newT.hr())
-
-                ))
+                  )
+                )
             });
 
         },
-
+        // Make a remote call to Twitter status for username
         remote : function( username, count ) {
             var url="https://twitter.com/status/user_timeline/%s.json?count=%d".replace(/%s/, username).replace(/%d/, count),
                 dfr=$.Deferred();
