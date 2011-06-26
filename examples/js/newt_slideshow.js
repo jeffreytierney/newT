c37afcbb7376613a61e05af1b80b52b9f7c62e59
gregory80 <gregory.tomlinson@gmail.com>
2011-06-26 11:14:34 -0400

    more js code for slideshow

--- examples/js/newt_slideshow.js
+++ examples/js/newt_slideshow.js
@@ -34,10 +34,11 @@
         // returns deferred promise (aka a callback);
         search : function( txt ) {
             var self=this, dfr=$.Deferred(), promise;
+            var params=$.extend(true, {}, self.remote_setup, { text : txt } );
             
-            promise = self.remote( self.meta.url, self.remote_setup );
+            promise = self.remote( self.meta.url, params );
             promise.done( dfr.resolve );
-            self.remote_setup.text=txt;
+            
             return dfr.promise();
 
         },
@@ -64,7 +65,7 @@
             var promise = self.search( term || "cute kitten" );
             promise.done( function(data) {
                 self.dom_add_photos(elem, data);
-                dfr.resolve();
+                dfr.resolve(data);
             });
             return dfr.promise();
         },
@@ -72,15 +73,10 @@
             var self=this,
                 photos=self.parse_photos( data );
 
+            self.loading=photos.length;
             newT.render("slideshow_box", photos, {
                 el : elem 
             });
-/*            for(var i=0; i<photos.length; i++){*/
-                //self.loading+=1;
-                //newT.render(self.templates.photo, photos[i], {
-                    //el : elem
-                //});
-            /*}*/
         },
 
         dom_outer_box : function() {
@@ -98,7 +94,7 @@
                 p_params={},
                 photos=[];
             for(var i=0; i<lst.length; i++) {
-                p_params=$.extend({}, true, self.photo_setup, lst[i]);
+                p_params=$.extend(true, {}, self.photo_setup, lst[i]);
                 var p_url = self.photo_url( p_params );
                 photos.push({
                     photo : p_url,
@@ -109,15 +105,18 @@
             
         },
 
-        photo_loaded : function(e, photo_info) {
+        photo_loaded : function(e, elem, photo_info) {
             var self=this;
             self.loading-=1;
+            elem.setAttribute("width", elem.naturalWidth || elem.width );
+            elem.setAttribute("height", elem.naturalHeight || elem.height );
+            elem.className=elem.className + " loaded_complete";
             if(self.loading===0){
                 self.photo_load_complete();
             }
-            console.log(this, e, photo_info);
+            delete self;
         },
-
+        // EVENT, override
         photo_load_complete : function() {
             console.log("photos done");
         },
@@ -139,7 +138,7 @@
             img.className="unloaded_img";
             img.onload=(function(scope, meta){
                 return function(e){
-                    scope.photo_loaded.call(scope, e, meta );
+                    scope.photo_loaded.call(scope, e, this, meta);
                 }
             })(self, data);
             img.src=data.photo;
@@ -177,15 +176,23 @@ function out( str, options ) {
 })(window);
 
 /*
- *
+*  showControls.js
+*
 * */
 (function(window, undefined){
     
     var sC = function() {
         this.init();
-    }
+    }, active_transition=false;
     sC.prototype = {
+        key : {
+            LEFT : 37,
+            UP : 38,
+            RIGHT : 39,
+            DOWN : 40
+        },
         init : function() {
+            this.listen_keyboard();
         },
 
         loader : function( bool ) {
@@ -196,27 +203,77 @@ function out( str, options ) {
         listen: function() {
             // add the  DOM events to listen for photos, plus more
             this.listen_photo();
-        },
-
-        forward : function() {
-            // external control to move forward
-            //
-            console.log("play me", arguments);
-        },
-        back : function() {
             
+        },
+        
+        start : function() {
+            this.current().fadeIn('normal');
+        },
+        transition_complete : function( pos ) {
+            console.log("finished")
+        },
+        change_photo : function( incr ) {
+            if(active_transition) { return; }
+            var self=this,
+                $curr=this.current(),
+                $imgs = this.all(),
+                pos = $.inArray($curr.get(0), $imgs),
+                num=0;
+
+            active_transition=true;
+            $curr.fadeOut();
+
+            if(Math.abs(incr) === incr) {
+                num = ($imgs.length > pos+incr ) ? pos+incr : 0
+            } else {
+                num=(pos+incr >= 0 ) ? pos+incr : $imgs.length-1;
+            }
+
+            $imgs.eq( num ).fadeIn('slow', function(){
+                active_transition=false;
+                self.transition_complete( pos+incr );
+            });
+        
+        },
+        all : function() {
+            return $(".sng_photo_box img");
+        },
+        current : function() {
+            var $curr = $(".sng_photo_box img:visible"); 
+            if(!$curr || $curr.length <= 0) {
+                $curr=$(".sng_photo_box img:first");
+            }
+            return $curr;
         },
         listen_photo : function() {
             // listen for clicks events on the photo
             //
             var self=this;
             $(".sng_photo_box").bind("click", function(e){
-                self.forward();
+                // find where this element is in list,
+                // show "next" photo
+                self.change_photo(1);
             })
         },
 
         listen_keyboard : function() {
             // listen for the arrow keys plus VIM key bindings
+            var self=this;
+            $(document.body).bind("keydown", function(e){
+                switch(e.keyCode) {
+                    case self.key.LEFT:
+                        e.preventDefault();
+                        console.log("left arrow");
+                        self.change_photo(-1);
+                    break;
+
+                    case self.key.RIGHT:
+                        e.preventDefault();
+                        console.log("right arrow");
+                        self.change_photo(1);
+                    break;
+                }
+            })
         }
 
     }
